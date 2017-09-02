a28d5120b6c7cb5e028e49c8ccac322eeec67446
Jeremy Ayerst <jeremyayerst@gmail.com>
2017-09-02 16:03:47 -0500

    create pages

--- src/Apple.js
+++
@@ -1,9 +0,0 @@
-import Sprite from "./Sprite";
-
-class Apple extends Sprite {
-  constructor(gameRegion) {
-    super(gameRegion, 50, 50, "apple");
-  }
-}
-
-export default Apple;
--- src/SnakeSegment.js
+++
@@ -1,9 +0,0 @@
-import Sprite from "./Sprite";
-
-class SnakeSegment extends Sprite {
-  constructor(gameRegion) {
-    super(gameRegion, 50, 50, "snake");
-  }
-}
-
-export default SnakeSegment;
